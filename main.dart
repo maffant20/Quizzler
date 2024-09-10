295036124bc3a48f4e4bbc0c6203952fc35408f5
Muhammad Affan <115905693+maffant20@users.noreply.github.com>
2024-09-10 14:34:49 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'quizbrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain qbrain = QuizBrain();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> sk = [];
+  void checkAns(bool userPAns) {
+    bool correctAns = qbrain.getCorrectAns();
+    setState(() {
+      if (qbrain.isFinished() == true) {
+        Alert(
+          context: context,
+          title: "QUIZ IS FINISHED",
+          desc: "Thank You For Using This App",
+        ).show();
+        qbrain.reset();
+        sk = [];
+      } else {
+        if (userPAns == correctAns) {
+          sk.add(Icon(
+            Icons.check,
+            color: Colors.yellow,
+          ));
+        } else {
+          sk.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+      }
+      qbrain.next();
+    });
+  }
+
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
+  // List<bool> answers = [false, true, true];
+
+  // int qn = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                qbrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAns(true);
+
+                // print(qn); //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAns(false);
+                // print(qn); //The user picked false.
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: sk,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String qt;
+  bool qA;
+
+  Question(String q, bool a) {
+    qt = q;
+    qA = a;
+  }
+}
